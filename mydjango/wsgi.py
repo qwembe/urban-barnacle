9e0fe5ba820b88cbdf8f6849bc4d16e1a91fdc02
Андрей Вайгачёв <qwembe@gmail.com>
2022-03-24 17:25:32 +0300

    added wsgi

--- mydjango/wsgi.py
+++ mydjango/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = os.path.expanduser("~/mysite")
+if path not in sys.path:
+    sys.path.insert(0, path)
+os.environ["DJANGO_SETTINGS_MODULE"] = 'mysite.settings'
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mydjango.settings')
 
-application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
